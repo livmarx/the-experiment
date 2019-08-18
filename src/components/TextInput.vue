e5ef0315002dde14531ba3e68cbcca537fd83b48
Liv Marx <oehrenmarks@gmail.com>
2019-08-18 16:54:26 -0400

    add reuseable text input component

---
+++ src/components/TextInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <label>{{label}}</label>:
+    <br />
+    <input :type={type} v-model="inputData" @keyup="updateData" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TextInput',
+  props: ['label', 'inputData', 'type'],
+  data() {
+    return {};
+  },
+  methods: {
+    updateData() {
+      this.$emit('updateData', this.inputData);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
